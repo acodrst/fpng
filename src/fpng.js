a09e7e388c69f6c5e160eb298271549b159d68d1
ORNG <acodrst@mail.com>
2024-12-18 11:39:26 -0800

    smaller png

---
+++ src/fpng.js
@@ -0,0 +1,51 @@
+import { UPNG } from 'UPNG'
+console.log(UPNG)
+import * as px from "pxxl";
+const pf = px.Pxxl.Font.ParseBDF(Deno.readTextFileSync("assets/5x7.bdf"));
+function fpng(text) {
+  const f_sides = 2;
+  const f_tracks = 80;
+  const f_sectors = 18;
+  const f_bytes = 512;
+  const dat = new TextEncoder().encode(text);
+  const width = 1024;
+  const font_height = 7;
+  console.log(
+    `1.44 MB Floppy Disk Size=${f_sides * f_tracks * f_sectors * f_bytes}`,
+  );
+  const head = new Uint8Array(width * font_height).fill(255);
+  const dat_rows = (dat.length) % width == 0
+    ? (dat.length) / width
+    : Math.floor(dat.length / width) + 1;
+  const dat_fill = new Uint8Array(dat_rows * width - dat.length).fill(
+    32,
+  );
+  const flp_dat_arr = new Uint8Array([
+    ...head,
+    ...dat,
+    ...dat_fill,
+  ]);
+  const pixels = pf.getPixels(text);
+  for (const pixel of pixels) {
+    for (let i = 0; i < 4; i++) {
+      flp_dat_arr[
+        (pixel.x + pixel.y * width / 4) * 4 + i
+      ] = [0, 0, 0, 255][i];
+    }
+  }
+  const img=new Uint8Array(
+    UPNG.encodeLL(
+      [flp_dat_arr],
+      width / 4,
+      dat_rows + font_height,
+      3,
+      1,
+      8
+    ),
+  );
+  console.log(
+    `Floppy PNG Size=${img.length}`,
+  );
+  return img
+}
+export { fpng };
