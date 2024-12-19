d22ae582fe9551f47e706812a101870602300109
ORNG <acodrst@mail.com>
2024-12-18 16:13:08 -0800

    9

--- src/fpng.js
+++ src/fpng.js
@@ -8,7 +8,7 @@ function fpng(label,text) {
   const f_tracks = 80;
   const f_sectors = 18;
   const f_bytes = 512;
-  const dat = zstd.compress(new TextEncoder().encode(text),22);
+  const dat = zstd.compress(new TextEncoder().encode(text),9);
   const width = 1024;
   const font_height = 7;
   console.log(
